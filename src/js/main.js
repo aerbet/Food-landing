cd7136660406a3f5e02925aacf94beaa432bbb78
Erbol Zhaparov <ae270718@gmail.com>
2024-02-04 18:38:25 +0600

    Used classes in project, rendered menu-items by JS classes

--- src/js/main.js
+++ src/js/main.js
@@ -141,4 +141,83 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   
   window.addEventListener('scroll', showModalByScroll);
-})
+  
+  // Creating classes for cards
+  
+ class MyCard {
+   constructor(src, alt, title, descr, price, parentSection,) {
+     this.src = src;
+     this.alt = alt;
+     this.title = title;
+     this.descr = descr;
+     this.price = price;
+     this.parent = document.querySelector(parentSection);
+     this.transfer = 89;
+     this.changeToSom();
+   }
+   
+   changeToSom() {
+     this.price = this.price * this.transfer;
+   }
+   
+   render() {
+     const element = document.createElement('div');
+     element.innerHTML = `
+     <div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> сом/день</div>
+        </div>
+    </div>
+     `;
+     this.parent.append(element);
+   }
+ }
+  
+  new MyCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    5,
+    '.menu .container'
+  ).render();
+  
+  new MyCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    9,
+    '.menu .container'
+  ).render();
+  
+  new MyCard(
+    "img/tabs/post.jpg",
+    "postnoe",
+    'Меню “Постное”',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    7,
+    '.menu .container'
+  ).render();
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

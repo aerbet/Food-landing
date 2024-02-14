1addab0dbab411a1c62a349fedec86ad5855bc8a
Erbol Zhaparov <ae270718@gmail.com>
2024-02-14 22:11:29 +0600

    Added axios cdn, will change with npm, changed styles of cards, now they are more content friendly

--- src/js/main.js
+++ src/js/main.js
@@ -163,20 +163,22 @@ window.addEventListener('DOMContentLoaded', () => {
    render() {
      const element = document.createElement('div');
      if (this.classes.length === 0) {
-       this.element = 'menu__item';
+       this.element = 'col';
        element.classList.add(this.element);
      } else {
        this.classes.forEach(className => element.classList.add(className));
      }
      
      element.innerHTML = `
-      <img src=${this.src} alt=${this.alt}>
-      <h3 class="menu__item-subtitle">${this.title}</h3>
-      <div class="menu__item-descr">${this.descr}</div>
-      <div class="menu__item-divider"></div>
-      <div class="menu__item-price">
-          <div class="menu__item-cost">Цена:</div>
-          <div class="menu__item-total"><span>${this.price}</span> сом/день</div>
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> сом/день</div>
+        </div>
       </div>
      `;
      this.parent.append(element);
@@ -193,36 +195,19 @@ window.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   };
  
- /*getResource('http://localhost:3000/menu')
+/* getResource('http://localhost:3000/menu')
    .then(data => {
      data.forEach(({ img, alt, title, descr, price }) => {
-       new MyCard(img, alt, title, descr, price, '.menu .container').render();
+       new MyCard(img, alt, title, descr, price, '.menu .container .row').render();
      });
    });*/
   
-  getResource('http://localhost:3000/menu')
-    .then(data => createCard(data));
-  
-  function createCard(data) {
-    data.forEach(({ img, alt, title, descr, price }) => {
-      const element = document.createElement('div');
-      price = price * 89;
-      element.classList.add('menu__item')
-      
-      element.innerHTML = `
-        <img src=${img} alt=${alt}>
-        <h3 class="menu__item-subtitle">${title}</h3>
-        <div class="menu__item-descr">${descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${price}</span> сом/день</div>
-        </div>
-      `;
-      
-      document.querySelector('.menu .container').append(element);
-    });
-  }
+  axios.get('http://localhost:3000/menu')
+    .then(data => {
+      data.data.forEach(({ img, alt, title, descr, price }) => {
+        new MyCard(img, alt, title, descr, price, '.menu .container .row').render();
+      });
+    })
   
   // Form
   
